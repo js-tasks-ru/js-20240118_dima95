3d119b704dffcb87e5e07295e652db7a0ba0becd
Dmitry Kislitsin <d.kislitsin@mlmedicine.ru>
2024-02-03 00:40:11 +0800

    add tast invert obj

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj === 'undefined') {
+    return undefined
+  }
+  let result = {}
+  for (let key in  obj) {
+    result[obj[key]]=key
+  }
 
+  return result
 }
