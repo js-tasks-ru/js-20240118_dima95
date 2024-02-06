0f2cd46f14c8dce251cbb9c101376285f4246b21
Dmitry Kislitsin <d.kislitsin@mlmedicine.ru>
2024-02-07 01:05:33 +0800

    add task trim symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+   if (!string || size === 0) {
+     return ""
+   }
+
+   if(![...arguments].find((element) => element === size)){
+     return string
+   }
+  let result = '';
+  let currentChar = '';
+  let currentCount = 0;
+
+  for (const char of string) {
+    if (char !== currentChar) {
+      currentChar = char;
+      currentCount = 1;
+      result += char;
+    } else {
+      currentCount++;
+      if (currentCount <= size) {
+        result += char;
+      }
+    }
+  }
+
+  return result;
 
 }
